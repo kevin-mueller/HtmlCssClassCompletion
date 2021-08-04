6d0c643203184c381fc2b03068128bfcfb8b8f86
Kevin Müller <kevin.mueller1@outlook.com>
2021-08-04 15:03:47 +0200

    minor changes

--- src/JsonElementCompletion/ElementCatalog.cs
+++ src/JsonElementCompletion/ElementCatalog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EnvDTE;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
@@ -10,13 +11,35 @@ namespace HtmlCssClassCompletion.JsonElementCompletion
     [Export]
     internal class ElementCatalog
     {
+        [Import]
+        DTEClass DTE;
+
         public List<CssClass> Classes { get; } = new List<CssClass>()
         {
             new CssClass("test-class", "testfile1.css")
         };
 
+        internal ElementCatalog()
+        {
+            RefreshClasses();
+        }
+
+        public void RefreshClasses()
+        {
+            
+
+            foreach (var item in DTE.Solution.Projects)
+            {
+                var projectPath = ((Project)item).FullName;
+            }
+            //TODO:
+            //search the entire project structure (with referenced projects!) for .css files and parse the classes from it.
+            //also search for <link> attributes from .html / .cshtml files and parse their classes as well.
+        }
+
         public class CssClass
         {
+            
             public string Name { get; }
             public string FileName { get; }
 
@@ -24,13 +47,6 @@ namespace HtmlCssClassCompletion.JsonElementCompletion
             {
                 Name = name;
                 FileName = fileName;
-            }
-
-            public void RefreshClasses()
-            {
-                //TODO:
-                //search the entire project structure (with referenced projects!) for .css files and parse the classes from it.
-                //also search for <link> attributes from .html / .cshtml files and parse their classes as well.
             }
         }
     }
